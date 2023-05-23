0f0ee77716810e029e1b630927cb8f622721fae8
Ihar Shumilouski <ihar.shumilouski@livewire.com>
2023-05-23 22:30:46 +0300

    task-8

--- src/cart/models/index.ts
+++ src/cart/models/index.ts
@@ -1,8 +1,5 @@
 export type Product = {
   id: string,
-  title: string,
-  description: string,
-  price: number,
 };
 
 
@@ -14,4 +11,18 @@ export type CartItem = {
 export type Cart = {
   id: string,
   items: CartItem[],
+}
+
+export type DBCart = {
+  id: string;
+  user_id: string;
+  created_at: Date;
+  updated_at: Date;
+  status: 'OPEN' | 'ORDERED';
+}
+
+export type DBCartItem = {
+  cart_id: string;
+  product_id: string;
+  count: number;
 }
